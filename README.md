c9a3702d9f7f746a08228672183cbddb705a099f
ecsavigne <ecsavigne@gmail.com>
2025-09-22 11:10:28 -0300

    update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # quasarwind
 
-1. install 
+### 1. install 
         npm i -g quasarwind
-2. execute cli
+### 2. execute cli
         quasarwind name_project
-3. Follow instructions
+### 3. Follow instructions
--- template/src/components/ExampleComponent.vue
+++ template/src/components/ExampleComponent.vue
@@ -1,64 +1,66 @@
 <template>
-  <div class="q-pa-md q-gutter-md grid grid-cols-4">
-    <q-card class="my-card">
-      <q-card-section>
-        {{ lorem }}
-      </q-card-section>
-    </q-card>
+ <div>
+    <div class="pl-[10px] text-3xl font-bold underline text-white">
+      Hello world 1! - Example aplication
+    </div>
 
-    <q-card
-      class="my-card text-white"
-      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
-    >
-      <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
+    <div class="q-pa-md q-gutter-md grid grid-cols-4">
+      <q-card class="my-card">
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
 
-      <q-card-section class="q-pt-none">21323
-        {{ lorem }}
-      </q-card-section>
-    </q-card>
+      <q-card
+        class="my-card text-white"
+        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
 
-    <q-card dark bordered class="bg-grey-9 my-card">
-      <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
+        <q-card-section class="q-pt-none">21323
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
 
-      <q-separator dark inset />
+      <q-card dark bordered class="bg-grey-9 my-card">
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
 
-      <q-card-section>
-        {{ lorem }}
-      </q-card-section>
-    </q-card>
+        <q-separator dark inset />
 
-    <q-card flat bordered class="my-card">
-      <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-      </q-card-section>
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
 
-      <q-card-section class="q-pt-none">
-        PPPP  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. dfsdfsadfsdfsddfsdfsdfsd
-      </q-card-section>
+      <q-card flat bordered class="my-card">
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+        </q-card-section>
 
-      <q-separator inset />
+        <q-card-section class="q-pt-none">
+          PPPP  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. dfsdfsadfsdfsddfsdfsdfsd
+        </q-card-section>
 
-      <q-card-section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.  sdfs
-      </q-card-section>
-    </q-card>
-  </div>
+        <q-separator inset />
 
-  <div class="text-3xl font-bold underline text-white">
-    Hello world 1! - Example aplication
-  </div>
+        <q-card-section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.  sdfs
+        </q-card-section>
+      </q-card>
+    </div>
 
-  <q-btn label="click me" color="primary" @click="store.increment">&nbsp; 
-    {{ store.doubleCount }}</q-btn>
-
+    <q-btn class="q-ml-md" label="click me" color="primary" @click="store.increment">&nbsp; 
+      {{ store.doubleCount }}
+    </q-btn>
+ </div>
 </template>
 
 <script setup lang="ts">
