c5ec6598e0d80ca8bba446719f13e659f81f09c2
ecsavigne <ecsavigne@gmail.com>
2026-01-21 23:58:58 -0300

    improve visual

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import { cwd, argv, exit } from 'node:process';
 import { spawn } from 'child_process';
+import * as colors from "./colors.js";
 
 // console.log("Dir: ", __dirname);
 const filesProject = path.join(import.meta.dirname, "../filesProject");
@@ -71,44 +72,49 @@ const runBuild = () => {
 // commands: [create(name_project), dev, build, help, version]
 const main = () => {
   const paramCommand = argv.slice(2)
-  // const projectName = paramCommand[1] || "my-project";
   targetDir = path.join(cwd(), projectName);
-  // console.log("targetDir: ", targetDir)
 
   if (paramCommand.length === 0) {
     showHelp()
+    exit(1);
   }
 
-  if (paramCommand[0] === "version" || paramCommand[0] === "-v") {
-    console.log(version);
-  }
-
-  if (paramCommand[0] === "help") {
-    showHelp()
-  }
-  
-  if (paramCommand[0] === "create") {
-    projectName = paramCommand[1]
-    if (!projectName) {
-      console.log("Ops! You need to specify a name for the project");
-      exit(1);
-    } else {
-      targetDir = path.join(cwd(), projectName); 
-      copyDir(filesProject, targetDir);
-      showInfo()
-    }
-  }
-
-  if (paramCommand[0] === "dev" || paramCommand[0] === "d") {
-    runDev()
-  }
-
-  if (paramCommand[0] === "build" || paramCommand[0] === "b") {
-    runBuild()
-  }
-  
-  if (paramCommand[0] === "clean" || paramCommand[0] === "c") {
-    runClean()
+  switch (paramCommand[0]) {
+    case "version":
+    case "-v":
+      console.log(version);
+    break;
+    case "help":
+    case "-h":
+      showHelp();
+    break;
+    case "clean":
+    case "c":
+      runClean();
+    break;
+    case "build":
+    case "b":
+      runBuild();
+    break;  
+    case "dev":
+    case "d":
+      runDev();
+    break;
+    case "create":
+      projectName = paramCommand[1]
+      if (!projectName) {
+        console.log("Ops! You need to specify a name for the project");
+        exit(1);
+      } else {
+        targetDir = path.join(cwd(), projectName); 
+        copyDir(filesProject, targetDir);
+        showInfo()
+      }
+    break;
+    default:
+      console.log(colors.Color(colors.red,"\nOps! Command not found\n"));
+      showHelp();
+    break;
   }
 } 
 
